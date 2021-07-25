4f9bf2d81ef8a7aa98135426edac6514e5fa7304
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-07-25 09:23:37 +0300

    Update mail templates

--- src/main/resources/db/migration/R__Update_mail_templates.sql
+++ src/main/resources/db/migration/R__Update_mail_templates.sql
@@ -3,5 +3,11 @@ delete from "messaging".mail_template;
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('ACCOUNT_ACTIVATION_TOKEN', 'Account activation', 'token-request', 'OpertusMundi', 'hello@OpertusMundi.eu', now())
+  ('ACCOUNT_ACTIVATION_TOKEN', 'Account activation', 'account-registration-activation-token', 'OpertusMundi', 'hello@OpertusMundi.eu', now())
+;
+
+insert into "messaging".mail_template (
+  "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
+) values
+  ('ACCOUNT_ACTIVATION_SUCCESS', 'Account activation completed', 'account-registration-complete', 'OpertusMundi', 'hello@OpertusMundi.eu', now())
 ;
