a4e2e5d86a697fd20467530b2d169a143158a3f3
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-07-25 09:25:45 +0300

    Add workflow fields to account table

---
+++ src/main/resources/db/migration/V1_0_48__Add_workflow_fields_to_account_table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "web".account ADD IF NOT EXISTS "registration_process_definition" character varying;
+ALTER TABLE "web".account ADD IF NOT EXISTS "registration_process_instance"   character varying;
