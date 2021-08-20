3cf505d46973d45a04f1615d4b8d7b136ec85405
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-08-20 14:10:03 +0300

    Update notification templates

--- src/main/resources/db/migration/R__Update_notification_templates.sql
+++ src/main/resources/db/migration/R__Update_notification_templates.sql
@@ -3,3 +3,7 @@ delete from "messaging".notification_template;
 insert into "messaging".notification_template ("type", "text", "modified_on") values
 ('CATALOGUE_HARVEST_COMPLETED', 'Harvest operation for catalogue {catalogueUrl} has been completed', now())
 ;
+
+insert into "messaging".notification_template ("type", "text", "modified_on") values
+('CATALOGUE_ASSET_UNPUBLISHED', 'Asset {assetName} {assetVersion} has been removed from the catalogue', now())
+;
