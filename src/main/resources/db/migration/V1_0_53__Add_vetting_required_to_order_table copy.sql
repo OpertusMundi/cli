905cbb0a0ccfd82553fc0036c6875cba9b59897f
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-09-02 04:09:05 +0300

    Update schema for consumer vetting and provider asset delivery

---
+++ src/main/resources/db/migration/V1_0_53__Add_vetting_required_to_order_table copy.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "order".order ADD IF NOT EXISTS "vetting_required" boolean NOT NULL DEFAULT false;
---
+++ src/main/resources/db/migration/V1_0_54__Add_provider_rejection_reason_to_order_table.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "order".order ADD IF NOT EXISTS "provider_rejection_reason" character varying NULL;
