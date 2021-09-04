c6977fafd19e27a5e02d52249a5f337a841d9b1c
nleond <nikleonidakis@gmail.com>
2021-09-04 11:59:21 +0300

    Insert record for purchase confirmation mail

--- src/main/resources/db/migration/R__Update_mail_templates.sql
+++ src/main/resources/db/migration/R__Update_mail_templates.sql
@@ -11,3 +11,9 @@ insert into "messaging".mail_template (
 ) values
   ('ACCOUNT_ACTIVATION_SUCCESS', 'Account activation completed', 'account-registration-complete', 'Topio', 'support@topio.market', now())
 ;
+
+insert into "messaging".mail_template (
+  "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
+) values
+  ('ORDER_CONFIRMATION', 'Order confirmation', 'asset-purchase-confirm', 'Topio', 'support@topio.market', now())
+;
