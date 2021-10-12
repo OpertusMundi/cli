4e411a9f49ecd851d8dc0529c9236167e24d0b48
vgsilis <vggeorgopoulos@gmail.com>
2021-10-12 17:02:11 +0300

    Insert records for notifications and change some email type names

--- src/main/resources/db/migration/R__Update_mail_templates.sql
+++ src/main/resources/db/migration/R__Update_mail_templates.sql
@@ -21,77 +21,77 @@ insert into "messaging".mail_template (
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('SUPPLIER_REGISTRATION_EVALUATION', 'Supplier registration evaluation', 'supplier-registration-evaluation', 'Topio', 'support@topio.market', now())
+  ('SUPPLIER_EVALUATION', 'Supplier registration evaluation', 'supplier-registration-evaluation', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('DELIVERY_REQUEST', 'Delivery request', 'supplier-deliver-asset-request', 'Topio', 'support@topio.market', now())
+  ('SUPPLIER_DELIVERY_REQUEST', 'Delivery request', 'supplier-deliver-asset-request', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('NEW_DATASET_PURCHASE', 'New dataset purchase', 'supplier-digital-delivery-by-platform', 'Topio', 'support@topio.market', now())
+  ('SUPPLIER_DIGITAL_DELIVERY', 'New dataset purchase', 'supplier-digital-delivery-by-platform', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('DATASET_PURCHASE_REQUEST', 'Dataset purchase request', 'supplier-purchase-reminder', 'Topio', 'support@topio.market', now())
+  ('SUPPLIER_PURCHASE_REMINDER', 'Dataset purchase request', 'supplier-purchase-reminder', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('DATASET_READY_FOR_DOWNLOAD_SUPPLIER', 'Dataset ready for download', 'consumer-digital-delivery-by-supplier', 'Topio', 'support@topio.market', now())
+  ('CONSUMER_DIGITAL_DELIVERY_BY_SUPPLIER', 'Dataset ready for download', 'consumer-digital-delivery-by-supplier', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('ORDER_SHIPPED', 'Order shipped', 'consumer-physical-delivery-by-supplier', 'Topio', 'support@topio.market', now())
+  ('CONSUMER_PHYSICAL_DELIVERY_BY_SUPPLIER', 'Order shipped', 'consumer-physical-delivery-by-supplier', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('DATASET_READY_FOR_DOWNLOAD_PLATFORM', 'Dataset ready for download', 'consumer-digital-delivery-by-platform', 'Topio', 'support@topio.market', now())
+  ('CONSUMER_DIGITAL_DELIVERY_BY_PLATFORM', 'Dataset ready for download', 'consumer-digital-delivery-by-platform', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('RECEIPT_ISSUED', 'Receipt issued', 'consumer-receipt-issued', 'Topio', 'support@topio.market', now())
+  ('CONSUMER_RECEIPT_ISSUED', 'Receipt issued', 'consumer-receipt-issued', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('PURCHASE_REQUEST_AWAITING_CONFIRMATION', 'Purchase request awaiting confirmation', 'consumer-purchase-notification', 'Topio', 'support@topio.market', now())
+  ('CONSUMER_PURCHASE_NOTIFICATION', 'Purchase request awaiting confirmation', 'consumer-purchase-notification', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('PURCHASE_REJECTED', 'Purchase rejected', 'consumer-purchase-rejected-by-supplier', 'Topio', 'support@topio.market', now())
+  ('CONSUMER_PURCHASE_REJECTION', 'Purchase rejected', 'consumer-purchase-rejected-by-supplier', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('UPLOAD_COMPLETED', 'Upload completed', 'files-upload-complete', 'Topio', 'support@topio.market', now())
+  ('FILES_UPLOAD_COMPLETED', 'Upload completed', 'files-upload-complete', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('NEW_MASTER_CONTRACT_TEMPLATE_AVAILABLE', 'New master contract template available', 'master-template-contract-update', 'Topio', 'support@topio.market', now())
+  ('MASTER_TEMPLATE_CONTRACT_UPDATE', 'New master contract template available', 'master-template-contract-update', 'Topio', 'support@topio.market', now())
 ;
 
 insert into "messaging".mail_template (
   "type", "subject_template", "content_template", "sender_name", "sender_email", "modified_on"
 ) values
-  ('IMPORTING_FROM_EXTERNAL_CATALOGUE_COMPLETED', 'Importing from external catalogue completed', 'catalogue-external-harvesting', 'Topio', 'support@topio.market', now())
+  ('CATALOGUE_HARVEST_COMPLETED', 'Importing from external catalogue completed', 'catalogue-external-harvesting', 'Topio', 'support@topio.market', now())
 ;
--- src/main/resources/db/migration/R__Update_notification_templates.sql
+++ src/main/resources/db/migration/R__Update_notification_templates.sql
@@ -7,3 +7,39 @@ insert into "messaging".notification_template ("type", "text", "modified_on") va
 insert into "messaging".notification_template ("type", "text", "modified_on") values
 ('CATALOGUE_ASSET_UNPUBLISHED', 'Asset {assetName} {assetVersion} has been removed from the catalogue', now())
 ;
+
+insert into "messaging".notification_template ("type", "text", "modified_on") values
+('ORDER_CONFIRMATION', 'Order {orderID} is being processed', now())
+;
+
+insert into "messaging".notification_template ("type", "text", "modified_on") values
+('DELIVERY_REQUEST', 'New delivery request for asset {assetName}', now())
+;
+
+insert into "messaging".notification_template ("type", "text", "modified_on") values
+('DIGITAL_DELIVERY', 'New purchase for asset {assetName}', now())
+;
+
+insert into "messaging".notification_template ("type", "text", "modified_on") values
+('PURCHASE_REMINDER', 'New purchase request for asset {assetName}', now())
+;
+
+insert into "messaging".notification_template ("type", "text", "modified_on") values
+('DIGITAL_DELIVERY_BY_SUPPLIER', 'Asset {assetName} is ready for download', now())
+;
+
+insert into "messaging".notification_template ("type", "text", "modified_on") values
+('PHYSICAL_DELIVERY_BY_SUPPLIER', 'Asset {assetName} has been shipped', now())
+;
+
+insert into "messaging".notification_template ("type", "text", "modified_on") values
+('DIGITAL_DELIVERY_BY_PLATFORM', 'Asset {assetName} is ready for download', now())
+;
+
+insert into "messaging".notification_template ("type", "text", "modified_on") values
+('PURCHASE_REJECTED', 'Purchase of asset {assetName} has been rejected by supplier {supplierName}', now())
+;
+
+insert into "messaging".notification_template ("type", "text", "modified_on") values
+('FILES_UPLOAD_COMPLETED', 'Files upload has been completed', now())
+;
