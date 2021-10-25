fc571cd0b850c5fc55fa6b98acaafb4195db8d4f
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-10-25 11:34:27 +0300

    Update asset metadata properties
    Added property `samples` for tabular assets.

--- src/main/resources/db/migration/R__Update_asset_metadata_properties.sql
+++ src/main/resources/db/migration/R__Update_asset_metadata_properties.sql
@@ -1,13 +1,14 @@
 delete from "provider".asset_metadata_property;
 
 insert into "provider".asset_metadata_property (asset_type, "name", "type") values
-  ('NETCDF', 'mbrStatic',        'PNG'),
-  ('RASTER', 'mbrStatic',        'PNG'),
-  ('VECTOR', 'clustersStatic',   'PNG'),
-  ('VECTOR', 'convexHullStatic', 'PNG'),
-  ('VECTOR', 'heatmap',          'JSON'),
-  ('VECTOR', 'heatmapStatic',    'PNG'),
-  ('VECTOR', 'mbrStatic',        'PNG'),
-  ('VECTOR', 'samples',          'JSON'),
-  ('VECTOR', 'thumbnail',        'PNG')
+  ('NETCDF',  'mbrStatic',        'PNG'),
+  ('RASTER',  'mbrStatic',        'PNG'),
+  ('TABULAR', 'samples',          'JSON'),
+  ('VECTOR',  'clustersStatic',   'PNG'),
+  ('VECTOR',  'convexHullStatic', 'PNG'),
+  ('VECTOR',  'heatmap',          'JSON'),
+  ('VECTOR',  'heatmapStatic',    'PNG'),
+  ('VECTOR',  'mbrStatic',        'PNG'),
+  ('VECTOR',  'samples',          'JSON'),
+  ('VECTOR',  'thumbnail',        'PNG')
  ;
