b27e808747108f1358b1105284e43700b70306a2
vgsilis <vggeorgopoulos@gmail.com>
2021-11-15 15:04:52 +0200

    Update notifications to support bold style

--- src/main/resources/db/migration/R__Update_notification_templates.sql
+++ src/main/resources/db/migration/R__Update_notification_templates.sql
@@ -1,47 +1,47 @@
 delete from "messaging".notification_template;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('CATALOGUE_HARVEST_COMPLETED', 'Harvest operation for catalogue {catalogueUrl} has been completed', now())
+('CATALOGUE_HARVEST_COMPLETED', 'Harvest operation for catalogue <span class="notification__text--bold">{catalogueUrl}</span> has been completed', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('CATALOGUE_ASSET_UNPUBLISHED', 'Asset {assetName} {assetVersion} has been removed from the catalogue', now())
+('CATALOGUE_ASSET_UNPUBLISHED', 'Asset <span class="notification__text--bold">{assetName}</span> <span class="notification__text--bold">{assetVersion}</span> has been removed from the catalogue', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('ORDER_CONFIRMATION', 'Order {orderId} is being processed', now())
+('ORDER_CONFIRMATION', 'Order <span class="notification__text--bold">{orderId}</span> is being processed', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('DELIVERY_REQUEST', 'New delivery request for asset {assetName}', now())
+('DELIVERY_REQUEST', 'New delivery request for asset <span class="notification__text--bold">{assetName}</span>', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('DIGITAL_DELIVERY', 'New purchase for asset {assetName}', now())
+('DIGITAL_DELIVERY', 'New purchase for asset <span class="notification__text--bold">{assetName}</span>', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('PURCHASE_REMINDER', 'New purchase request for asset {assetName}', now())
+('PURCHASE_REMINDER', 'New purchase request for asset <span class="notification__text--bold">{assetName}</span>', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('DIGITAL_DELIVERY_BY_SUPPLIER', 'Asset {assetName} is ready for download', now())
+('DIGITAL_DELIVERY_BY_SUPPLIER', 'Asset <span class="notification__text--bold">{assetName}</span> is ready for download', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('PHYSICAL_DELIVERY_BY_SUPPLIER', 'Asset {assetName} has been shipped', now())
+('PHYSICAL_DELIVERY_BY_SUPPLIER', 'Asset <span class="notification__text--bold">{assetName}</span> has been shipped', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('DIGITAL_DELIVERY_BY_PLATFORM', 'Asset {assetName} is ready for download', now())
+('DIGITAL_DELIVERY_BY_PLATFORM', 'Asset <span class="notification__text--bold">{assetName}</span> is ready for download', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('PURCHASE_APPROVED', 'Purchase of asset {assetName} has been approved by supplier {supplierName}', now())
+('PURCHASE_APPROVED', 'Purchase of asset <span class="notification__text--bold">{assetName}</span> has been approved by supplier <span class="notification__text--bold">{supplierName}</span>', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('PURCHASE_REJECTED', 'Purchase of asset {assetName} has been rejected by supplier {supplierName}', now())
+('PURCHASE_REJECTED', 'Purchase of asset <span class="notification__text--bold">{assetName}</span> has been rejected by supplier <span class="notification__text--bold">{supplierName}</span>', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
@@ -49,13 +49,13 @@ insert into "messaging".notification_template ("type", "text", "modified_on") va
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('ASSET_PUBLISHING_ACCEPTED', 'Publishing of {assetName} has been approved by the helpdesk', now())
+('ASSET_PUBLISHING_ACCEPTED', 'Publishing of <span class="notification__text--bold">{assetName}</span> has been approved by the Helpdesk', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('ASSET_PUBLISHING_REJECTED', 'Publishing of {assetName} has been rejected by the helpdesk', now())
+('ASSET_PUBLISHING_REJECTED', 'Publishing of <span class="notification__text--bold">{assetName}</span> has been rejected by the Helpdesk', now())
 ;
 
 insert into "messaging".notification_template ("type", "text", "modified_on") values
-('ASSET_PUBLISHED', 'Asset {assetName} has been successfully published', now())
+('ASSET_PUBLISHED', 'Asset <span class="notification__text--bold">{assetName}</span> has been successfully published', now())
 ;
