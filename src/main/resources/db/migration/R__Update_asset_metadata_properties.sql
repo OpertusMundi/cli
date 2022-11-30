524675f7daa7315ae40b366d8ad99d47fb25de16
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2022-11-30 16:29:01 +0200

    Update asset metadata properties

--- src/main/resources/db/migration/R__Update_asset_metadata_properties.sql
+++ src/main/resources/db/migration/R__Update_asset_metadata_properties.sql
@@ -2,9 +2,18 @@ delete from "provider".asset_metadata_property;
 
 insert into "provider".asset_metadata_property (asset_type, "name", "type") values
   ('NETCDF',  'mbrStatic',        'PNG'),
+
   ('RASTER',  'mbrStatic',        'PNG'),
   ('RASTER',  'thumbnail',        'PNG'),
+
+  ('TABULAR', 'clustersStatic',   'PNG'),
+  ('TABULAR', 'convexHullStatic', 'PNG'),
+  ('TABULAR', 'heatmap',          'JSON'),
+  ('TABULAR', 'heatmapStatic',    'PNG'),
+  ('TABULAR', 'mbrStatic',        'PNG'),
   ('TABULAR', 'samples',          'JSON'),
+  ('TABULAR', 'thumbnail',        'PNG'),
+
   ('VECTOR',  'clustersStatic',   'PNG'),
   ('VECTOR',  'convexHullStatic', 'PNG'),
   ('VECTOR',  'heatmap',          'JSON'),
