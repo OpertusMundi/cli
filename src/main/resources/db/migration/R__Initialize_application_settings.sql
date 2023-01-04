d0a5e83a23268fe027d2bef41f83534fbf1a9920
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2023-01-04 14:21:21 +0200

    Add column `read_only` to settings table

--- src/main/resources/db/migration/R__Initialize_application_settings.sql
+++ src/main/resources/db/migration/R__Initialize_application_settings.sql
@@ -1,5 +1,6 @@
-DELETE FROM web.settings;
-
-INSERT into web.settings values ('API_GATEWAY',   'announcement.text',                'HTML',     '',      null,  now()) ON CONFLICT DO NOTHING;
-INSERT into web.settings values ('API_GATEWAY',   'announcement.enabled',             'BOOLEAN',  'false', null,  now()) ON CONFLICT DO NOTHING;
-INSERT into web.settings values ('ADMIN_GATEWAY', 'user-service.pricing-model.price', 'JSON',  '',  null,  now()) ON CONFLICT DO NOTHING;
+INSERT into web.settings values ('API_GATEWAY',   'announcement.text',                'HTML',     '',      null,  now(), false)
+  ON CONFLICT DO NOTHING;
+INSERT into web.settings values ('API_GATEWAY',   'announcement.enabled',             'BOOLEAN',  'false', null,  now(), false)
+  ON CONFLICT DO NOTHING;
+INSERT into web.settings values ('ADMIN_GATEWAY', 'user-service.pricing-model.price', 'JSON',     '',      null,  now(), false)
+  ON CONFLICT DO NOTHING;
